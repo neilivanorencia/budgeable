75087d584fbcc30fd0cf84cdb4766a36a433d9eb
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-04-01 07:48:25 +0800

    Configure fonts with Inter and Manrope

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,18 @@
 import type { Metadata } from "next";
 import "@/app/globals.css";
 
-import { Inter } from "next/font/google";
+import { Inter, Manrope } from "next/font/google";
+
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({
   subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+const manrope = Manrope({
+  subsets: ["latin"],
+  variable: "--font-manrope",
 });
 
 export const metadata: Metadata = {
@@ -18,8 +26,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        layout: {
+          unsafe_disableDevelopmentModeWarnings: true,
+        },
+      }}
+    >
+      <html lang="en">
+        <body className={`${inter.variable} ${manrope.variable} antialiased`}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
