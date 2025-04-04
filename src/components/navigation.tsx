95cbd9d405dc226d1628ba94b9aa64c8f138c42b
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-04-05 05:21:48 +0800

    Integrate routing hooks in the navigation

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,9 +1,13 @@
 "use client";
 
+import { usePathname, useRouter } from "next/navigation";
 import { useState } from "react";
 
 export const Navigation = () => {
   const [isOpen, setIsOpen] = useState(false);
+
+  const router = useRouter();
+  const pathname = usePathname();
 
   return <div></div>;
 };
