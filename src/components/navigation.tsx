83d0b1ae75850a003269dadbd5ac04902d65e496
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-04-05 05:41:38 +0800

    Configure route navigation structure

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -2,7 +2,39 @@
 
 import { usePathname, useRouter } from "next/navigation";
 import { useState } from "react";
+import { IoPersonOutline, IoSettingsOutline } from "react-icons/io5";
+import { LuLayoutDashboard } from "react-icons/lu";
+import { PiMoney } from "react-icons/pi";
+import { TbTransfer } from "react-icons/tb";
 import { useMedia } from "react-use";
+
+const routes = [
+  {
+    href: "/",
+    label: "Dashboard",
+    icon: LuLayoutDashboard,
+  },
+  {
+    href: "/transactions",
+    label: "Transactions",
+    icon: TbTransfer,
+  },
+  {
+    href: "/accounts",
+    label: "Accounts",
+    icon: IoPersonOutline,
+  },
+  {
+    href: "/categories",
+    label: "Categories",
+    icon: PiMoney,
+  },
+  {
+    href: "/settings",
+    label: "Settings",
+    icon: IoSettingsOutline,
+  },
+];
 
 export const Navigation = () => {
   const [isOpen, setIsOpen] = useState(false);
