d036f7c674431b2a782aa6e3c8f90d91f1b97e72
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-04-05 07:42:39 +0800

    Build header structure with previous components

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,11 +1,5 @@
-import { UserButton } from "@clerk/nextjs";
-
 const DashboardPage = () => {
-  return (
-    <div>
-      <UserButton />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default DashboardPage;
---
+++ src/components/header.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { Loader2 } from "lucide-react";
+
+import { HeaderLogo } from "@/components/header-logo";
+import { Navigation } from "@/components/navigation";
+import { WelcomeMessage } from "@/components/welcome-message";
+import { ClerkLoaded, ClerkLoading, UserButton, useUser } from "@clerk/nextjs";
+
+export const Header = () => {
+  const { user } = useUser();
+
+  return (
+    <header className="bg-teal-600 bg-[url('/topography.svg')] bg-repeat px-4 py-8 pb-36 bg-blend-soft-light lg:px-14">
+      <div className="mx-auto max-w-screen-2xl">
+        <div className="mb-14 flex w-full items-center justify-between">
+          <div className="flex items-center gap-x-4 lg:hidden">
+            <div className="hidden lg:block">
+              <HeaderLogo />
+            </div>
+            <Navigation />
+          </div>
+
+          <div className="hidden lg:block">
+            <HeaderLogo />
+          </div>
+
+          <div className="hidden flex-1 justify-center lg:flex">
+            <Navigation />
+          </div>
+
+          <div className="flex items-center gap-x-4">
+            <ClerkLoaded>
+              <span className="hidden text-base font-medium text-slate-200 md:inline">
+                {user?.fullName}
+              </span>
+              <UserButton />
+            </ClerkLoaded>
+            <ClerkLoading>
+              <Loader2 className="size-8 animate-spin text-slate-200" />
+            </ClerkLoading>
+          </div>
+        </div>
+        <WelcomeMessage />
+      </div>
+    </header>
+  );
+};
