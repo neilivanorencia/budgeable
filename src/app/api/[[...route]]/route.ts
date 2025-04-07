8b9ce8461c2138c905b28d668788d19063e09b43
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-04-07 09:52:32 +0800

    Setup accounts API infrastructure

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,9 +1,16 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
+import accounts from "@/app/api/[[...route]]/accounts";
+
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const routes = app.route("/accounts", accounts);
+
 export const GET = handle(app);
 export const POST = handle(app);
+
+export type AppType = typeof routes;
