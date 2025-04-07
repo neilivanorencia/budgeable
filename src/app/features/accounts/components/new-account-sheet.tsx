8befe8c6ce46692b9a99612c0cde32bad36dcdc8
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-04-08 07:53:26 +0800

    Implement new account creation with form validation

--- src/app/features/accounts/components/new-account-sheet.tsx
+++ src/app/features/accounts/components/new-account-sheet.tsx
@@ -1,3 +1,7 @@
+import { z } from "zod";
+
+import { useCreateAccount } from "@/app/features/accounts/api/use-create-account";
+import { AccountForm } from "@/app/features/accounts/components/account-form";
 import { useNewAccount } from "@/app/features/accounts/hooks/use-new-account";
 import {
   Sheet,
@@ -6,19 +10,45 @@ import {
   SheetHeader,
   SheetTitle,
 } from "@/components/ui/sheet";
+import { accountsInsertSchema } from "@/db/schema";
+
+const formSchema = accountsInsertSchema.pick({
+  name: true,
+});
+
+type FormValues = z.input<typeof formSchema>;
 
 export const NewAccountSheet = () => {
   const { isOpen, onClose } = useNewAccount();
+  const mutation = useCreateAccount();
+
+  const onSubmit = (values: FormValues) => {
+    const validated = formSchema.parse(values);
+    mutation.mutate(validated, {
+      onSuccess: () => {
+        onClose();
+      },
+    });
+  };
 
   return (
     <Sheet open={isOpen} onOpenChange={onClose}>
-      <SheetContent className="space-y-4 bg-teal-50">
+      <SheetContent className="space-y-4 bg-teal-50 px-4 pt-6">
         <SheetHeader>
-          <SheetTitle className="text-center">New Account</SheetTitle>
+          <SheetTitle className="font-manrope text-center text-xl font-bold">
+            New Account
+          </SheetTitle>
           <SheetDescription className="text-center">
             Start tracking your finances by adding a new account.
           </SheetDescription>
         </SheetHeader>
+        <AccountForm
+          onSubmit={onSubmit}
+          disabled={mutation.isPending}
+          defaultValues={{
+            name: "",
+          }}
+        />
       </SheetContent>
     </Sheet>
   );
