8ce990c7cb1c3ac2316aa349c4579546cc516bdc
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-04-09 10:58:47 +0800

    Implement accounts page with data table columns

---
+++ src/app/(dashboard)/accounts/columns.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { InferResponseType } from "hono";
+import { ArrowUpDown } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { client } from "@/lib/hono";
+import { ColumnDef } from "@tanstack/react-table";
+
+export type ResponseType = InferResponseType<typeof client.api.accounts.$get, 200>["data"][0];
+
+export const columns: ColumnDef<ResponseType>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <div className="flex items-center justify-center">
+        <Checkbox
+          checked={table.getIsAllPageRowsSelected()}
+          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+          aria-label="Select all"
+        />
+      </div>
+    ),
+    cell: ({ row }) => (
+      <div className="flex items-center justify-center">
+        <Checkbox
+          checked={row.getIsSelected()}
+          onCheckedChange={(value) => row.toggleSelected(!!value)}
+          aria-label="Select row"
+        />
+      </div>
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "name",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Name
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+];
---
+++ src/app/(dashboard)/accounts/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { LuPlus } from "react-icons/lu";
+
+import { columns } from "@/app/(dashboard)/accounts/columns";
+import { DataTable } from "@/components/data-table";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+
+const AccountsPage = () => {
+  const newAccount = useNewAccount();
+
+  return (
+    <div className="mx-auto -mt-24 w-full max-w-screen-2xl pb-10">
+      <Card className="border-none shadow-none drop-shadow-none">
+        <CardHeader className="flex flex-col items-center gap-y-2 md:flex-row md:items-center md:justify-between">
+          <CardTitle className="font-manrope line-clamp-1 text-lg font-semibold text-slate-800 md:text-xl">
+            Accounts Page
+          </CardTitle>
+          <Button
+            className="transition-color w-full cursor-pointer bg-teal-500 shadow-none duration-300 ease-in-out hover:bg-teal-400 hover:shadow-lg hover:shadow-teal-200/50 md:w-auto"
+            onClick={newAccount.onOpen}
+          >
+            <LuPlus className="size-4" />
+            Add new
+          </Button>
+        </CardHeader>
+        <CardContent>
+          <DataTable columns={columns} data={[]} filterKey="name" onDelete={() => {}} />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default AccountsPage;
