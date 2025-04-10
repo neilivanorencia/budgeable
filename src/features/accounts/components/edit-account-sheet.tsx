7b3c5d2024fc53c04624bc96608e86b8589b0607
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-04-11 06:48:44 +0800

    Implement account data loading and edit account sheet

--- src/features/accounts/components/edit-account-sheet.tsx
+++ src/features/accounts/components/edit-account-sheet.tsx
@@ -1,7 +1,6 @@
+import { Loader2 } from "lucide-react";
 import { z } from "zod";
 
-import { useCreateAccount } from "@/features/accounts/api/use-create-account";
-import { AccountForm } from "@/features/accounts/components/account-form";
 import {
   Sheet,
   SheetContent,
@@ -10,8 +9,10 @@ import {
   SheetTitle,
 } from "@/components/ui/sheet";
 import { accountsInsertSchema } from "@/db/schema";
-import { useOpenAccount } from "@/features/accounts/hooks/use-open-account";
+import { useCreateAccount } from "@/features/accounts/api/use-create-account";
 import { useGetAccount } from "@/features/accounts/api/use-get-account";
+import { AccountForm } from "@/features/accounts/components/account-form";
+import { useOpenAccount } from "@/features/accounts/hooks/use-open-account";
 
 const formSchema = accountsInsertSchema.pick({
   name: true,
@@ -25,6 +26,8 @@ export const EditAccountSheet = () => {
   const accountQuery = useGetAccount(id);
   const mutation = useCreateAccount();
 
+  const isLoading = accountQuery.isLoading;
+
   const onSubmit = (values: FormValues) => {
     const validated = formSchema.parse(values);
     mutation.mutate(validated, {
@@ -34,28 +37,37 @@ export const EditAccountSheet = () => {
     });
   };
 
-  const defaultValues = accountQuery.data ? {
-    name: accountQuery.data.name,
-  } : {
-    name: "",
-  }
+  const defaultValues = accountQuery.data
+    ? {
+        name: accountQuery.data.name,
+      }
+    : {
+        name: "",
+      };
 
   return (
     <Sheet open={isOpen} onOpenChange={onClose}>
       <SheetContent className="space-y-4 bg-teal-50 px-4 pt-6">
         <SheetHeader>
           <SheetTitle className="font-manrope text-center text-xl font-bold">
-            New Account
+            Edit Account
           </SheetTitle>
           <SheetDescription className="text-center">
-            Start tracking your finances by adding a new account.
+            Update an existing account details.
           </SheetDescription>
         </SheetHeader>
-        <AccountForm
-          onSubmit={onSubmit}
-          disabled={mutation.isPending}
-          defaultValues={defaultValues}
-        />
+        {isLoading ? (
+          <div className="absolute-inset-0 flex items-center justify-center">
+            <Loader2 className="text-muted-foreground size-8 animate-spin" />
+          </div>
+        ) : (
+          <AccountForm
+            id={id}
+            onSubmit={onSubmit}
+            disabled={mutation.isPending}
+            defaultValues={defaultValues}
+          />
+        )}
       </SheetContent>
     </Sheet>
   );
