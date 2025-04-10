b7f4fc013046d159ef2e1f92b9fbef2b7052a839
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-04-11 07:46:15 +0800

    Implement account deletion from dropdown menu

--- src/app/(dashboard)/accounts/actions.tsx
+++ src/app/(dashboard)/accounts/actions.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { MoreHorizontal } from "lucide-react";
+import { BsTrash } from "react-icons/bs";
 import { SlPencil } from "react-icons/sl";
 
 import { Button } from "@/components/ui/button";
@@ -10,7 +11,9 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { useDeleteAccount } from "@/features/accounts/api/use-delete-account";
 import { useOpenAccount } from "@/features/accounts/hooks/use-open-account";
+import { useConfirm } from "@/hooks/use-confirm";
 
 type Props = {
   id: string;
@@ -19,9 +22,24 @@ type Props = {
 export const Actions = ({ id }: Props) => {
   const { onOpen } = useOpenAccount();
 
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Are you sure?",
+    "Deleting this account cannot be undone."
+  );
+
+  const deleteMutation = useDeleteAccount(id);
+  const handleDelete = async () => {
+    const confirmed = await confirm();
+
+    if (confirmed) {
+      deleteMutation.mutate();
+    }
+  };
+
   return (
     <div>
       <>
+        <ConfirmDialog />
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <Button variant="ghost" className="size-8 p-0">
@@ -29,9 +47,13 @@ export const Actions = ({ id }: Props) => {
             </Button>
           </DropdownMenuTrigger>
           <DropdownMenuContent align="end">
-            <DropdownMenuItem disabled={false} onClick={() => onOpen(id)}>
+            <DropdownMenuItem disabled={deleteMutation.isPending} onClick={() => onOpen(id)}>
               <SlPencil className="text-slate-800" />
               Edit
+            </DropdownMenuItem>
+            <DropdownMenuItem disabled={deleteMutation.isPending} onClick={handleDelete}>
+              <BsTrash className="text-slate-800" />
+              Delete
             </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
