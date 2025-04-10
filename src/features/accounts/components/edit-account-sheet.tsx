f87e388ec334d659bd0d3dfbc2b6201e8f77577a
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-04-11 07:29:32 +0800

    Implement confirmation modal when deleting account

--- src/features/accounts/components/edit-account-sheet.tsx
+++ src/features/accounts/components/edit-account-sheet.tsx
@@ -14,6 +14,7 @@ import { useEditAccount } from "@/features/accounts/api/use-edit-account";
 import { useGetAccount } from "@/features/accounts/api/use-get-account";
 import { AccountForm } from "@/features/accounts/components/account-form";
 import { useOpenAccount } from "@/features/accounts/hooks/use-open-account";
+import { useConfirm } from "@/hooks/use-confirm";
 
 const formSchema = accountsInsertSchema.pick({
   name: true,
@@ -23,6 +24,11 @@ type FormValues = z.input<typeof formSchema>;
 
 export const EditAccountSheet = () => {
   const { isOpen, onClose, id } = useOpenAccount();
+
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Are you sure you?",
+    "Deleting an account cannot be undone."
+  );
 
   const accountQuery = useGetAccount(id);
   const editMutation = useEditAccount();
@@ -46,6 +52,18 @@ export const EditAccountSheet = () => {
     );
   };
 
+  const onDelete = async () => {
+    const confirmed = await confirm();
+
+    if (confirmed) {
+      deleteMutation.mutate(undefined, {
+        onSuccess: () => {
+          onClose();
+        },
+      });
+    }
+  };
+
   const defaultValues = accountQuery.data
     ? {
         name: accountQuery.data.name,
@@ -55,30 +73,33 @@ export const EditAccountSheet = () => {
       };
 
   return (
-    <Sheet open={isOpen} onOpenChange={onClose}>
-      <SheetContent className="space-y-4 bg-teal-50 px-4 pt-6">
-        <SheetHeader>
-          <SheetTitle className="font-manrope text-center text-xl font-bold">
-            Edit Account
-          </SheetTitle>
-          <SheetDescription className="text-center">
-            Update an existing account details.
-          </SheetDescription>
-        </SheetHeader>
-        {isLoading ? (
-          <div className="absolute-inset-0 flex items-center justify-center">
-            <Loader2 className="text-muted-foreground size-8 animate-spin" />
-          </div>
-        ) : (
-          <AccountForm
-            id={id}
-            onSubmit={onSubmit}
-            disabled={isPending}
-            defaultValues={defaultValues}
-            onDelete={deleteMutation.mutate}
-          />
-        )}
-      </SheetContent>
-    </Sheet>
+    <>
+      <ConfirmDialog />
+      <Sheet open={isOpen} onOpenChange={onClose}>
+        <SheetContent className="space-y-4 bg-teal-50 px-4 pt-6">
+          <SheetHeader>
+            <SheetTitle className="font-manrope text-center text-xl font-bold">
+              Edit Account
+            </SheetTitle>
+            <SheetDescription className="text-center">
+              Update an existing account details.
+            </SheetDescription>
+          </SheetHeader>
+          {isLoading ? (
+            <div className="absolute-inset-0 flex items-center justify-center">
+              <Loader2 className="text-muted-foreground size-8 animate-spin" />
+            </div>
+          ) : (
+            <AccountForm
+              id={id}
+              onSubmit={onSubmit}
+              disabled={isPending}
+              defaultValues={defaultValues}
+              onDelete={onDelete}
+            />
+          )}
+        </SheetContent>
+      </Sheet>
+    </>
   );
 };
