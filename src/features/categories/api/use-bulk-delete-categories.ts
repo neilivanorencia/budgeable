1d37ca474372cf0225599340ce970ecc37c75b8e
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-04-12 13:14:15 +0800

    Implement category data management hooks

---
+++ src/features/categories/api/use-bulk-delete-categories.ts
@@ -0,0 +1,29 @@
+import { InferRequestType, InferResponseType } from "hono";
+import { toast } from "sonner";
+
+import { client } from "@/lib/hono";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+type ResponseType = InferResponseType<(typeof client.api.accounts)["bulk-delete"]["$post"]>;
+type RequestType = InferRequestType<(typeof client.api.accounts)["bulk-delete"]["$post"]>["json"];
+
+export const useBulkDeleteCategories = () => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async (json) => {
+      const response = await client.api.categories["bulk-delete"].$post({ json });
+
+      return await response.json();
+    },
+    onSuccess: () => {
+      toast.success("Categories deleted");
+      queryClient.invalidateQueries({ queryKey: ["categories"] });
+    },
+    onError: () => {
+      toast.error("Failed to delete categories");
+    },
+  });
+
+  return mutation;
+};
---
+++ src/features/categories/api/use-edit-category.ts
@@ -0,0 +1,33 @@
+import { InferRequestType, InferResponseType } from "hono";
+import { toast } from "sonner";
+
+import { client } from "@/lib/hono";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+type ResponseType = InferResponseType<(typeof client.api.categories)[":id"]["$patch"]>;
+type RequestType = InferRequestType<(typeof client.api.categories)[":id"]["$patch"]>["json"];
+
+export const useEditCategory = () => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<ResponseType, Error, { id: string; data: RequestType }>({
+    mutationFn: async ({ id, data }) => {
+      const response = await client.api.categories[":id"]["$patch"]({
+        param: { id },
+        json: data,
+      });
+
+      return await response.json();
+    },
+    onSuccess: (_, { id }) => {
+      toast.success("Category edited");
+      queryClient.invalidateQueries({ queryKey: ["category", { id }] });
+      queryClient.invalidateQueries({ queryKey: ["categories"] });
+    },
+    onError: () => {
+      toast.error("Failed to edit category");
+    },
+  });
+
+  return mutation;
+};
