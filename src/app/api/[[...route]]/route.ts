bb3de6298ff6d4a624bd11b07ce922e53705a0c5
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-04-13 04:48:44 +0800

    Create and implement transactions CRUD endpoints

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -3,13 +3,17 @@ import { handle } from "hono/vercel";
 
 import accounts from "@/app/api/[[...route]]/accounts";
 import categories from "@/app/api/[[...route]]/categories";
+import transactions from "@/app/api/[[...route]]/transactions";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const routes = app.route("/accounts", accounts).route("/categories", categories);
+const routes = app
+  .route("/accounts", accounts)
+  .route("/categories", categories)
+  .route("/transactions", transactions);
 
 export const GET = handle(app);
 export const POST = handle(app);
