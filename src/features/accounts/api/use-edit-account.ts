1c36781249cfc823d8138a959cbee6bf5b434e33
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-04-12 13:19:21 +0800

    Correct query key for single account validation

--- src/features/accounts/api/use-edit-account.ts
+++ src/features/accounts/api/use-edit-account.ts
@@ -21,7 +21,7 @@ export const useEditAccount = () => {
     },
     onSuccess: (_, { id }) => {
       toast.success("Account edited");
-      queryClient.invalidateQueries({ queryKey: ["accounts", { id }] });
+      queryClient.invalidateQueries({ queryKey: ["account", { id }] });
       queryClient.invalidateQueries({ queryKey: ["accounts"] });
     },
     onError: () => {
