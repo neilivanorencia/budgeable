2a9298b7fac6c81918766bb385a24d5db0cdb31d
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-04-12 12:28:49 +0800

    Create categories table schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -9,3 +9,10 @@ export const accounts = pgTable("accounts", {
 });
 
 export const accountsInsertSchema = createInsertSchema(accounts);
+
+export const categories = pgTable("categories", {
+  id: text("id").primaryKey(),
+  plaidId: text("plaid_id"),
+  name: text("name").notNull(),
+  userId: text("user_id").notNull(),
+});
