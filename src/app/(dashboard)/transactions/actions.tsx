659fd8d7f5656d0896dc5a8d0f6150e71aaea6b6
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-04-16 07:53:31 +0800

    Add transaction actions with CRUD functionality

---
+++ src/app/(dashboard)/transactions/actions.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { MoreHorizontal } from "lucide-react";
+import { BsTrash } from "react-icons/bs";
+import { SlPencil } from "react-icons/sl";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useDeleteTransaction } from "@/features/transactions/api/use-delete-transaction";
+import { useOpenTransaction } from "@/features/transactions/hooks/use-open-transaction";
+import { useConfirm } from "@/hooks/use-confirm";
+
+type Props = {
+  id: string;
+};
+
+export const Actions = ({ id }: Props) => {
+  const { onOpen } = useOpenTransaction();
+
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Are you sure?",
+    "Deleting this transaction cannot be undone."
+  );
+
+  const deleteMutation = useDeleteTransaction(id);
+  const handleDelete = async () => {
+    const confirmed = await confirm();
+
+    if (confirmed) {
+      deleteMutation.mutate();
+    }
+  };
+
+  return (
+    <div>
+      <>
+        <ConfirmDialog />
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="size-8 p-0">
+              <MoreHorizontal />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem disabled={deleteMutation.isPending} onClick={() => onOpen(id)}>
+              <SlPencil className="text-slate-800" />
+              Edit
+            </DropdownMenuItem>
+            <DropdownMenuItem disabled={deleteMutation.isPending} onClick={handleDelete}>
+              <BsTrash className="text-slate-800" />
+              Delete
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </>
+    </div>
+  );
+};
