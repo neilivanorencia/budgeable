e5d56a9f52f7e56e777f67e5883143354092912d
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-04-19 13:32:37 +0800

    Create bar chart variant using recharts

---
+++ src/components/bar-variant.tsx
@@ -0,0 +1,33 @@
+import { format } from "date-fns";
+import { Bar, BarChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis } from "recharts";
+
+import { CustomTooltip } from "@/components/custom-tooltip";
+
+type Props = {
+  data: {
+    date: string;
+    income: number;
+    expenses: number;
+  }[];
+};
+
+export const BarVariant = ({ data }: Props) => {
+  return (
+    <ResponsiveContainer width="100%" height={350}>
+      <BarChart data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis
+          axisLine={false}
+          tickLine={false}
+          dataKey="date"
+          tickFormatter={(value) => format(value, "dd MMM")}
+          tickMargin={16}
+          style={{ fontSize: "12px" }}
+        />
+        <Tooltip content={<CustomTooltip />} />
+        <Bar dataKey="income" fill="#14b8a6" className="drop-shadow-sm" />
+        <Bar dataKey="expenses" fill="#ec4899" className="drop-shadow-sm" />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
