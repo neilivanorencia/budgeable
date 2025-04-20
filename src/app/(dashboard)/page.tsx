8481a4fca9f4cfbb313422a04d115a2902da0dde
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-04-20 10:00:11 +0800

    Integrate data chart component to the dashboard page

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,9 +1,11 @@
+import { DataChart } from "@/components/data-chart";
 import { DataGrid } from "@/components/data-grid";
 
 export default function DashboardPage() {
   return (
     <div className="mx-auto -mt-24 w-full max-w-screen-2xl pb-10">
       <DataGrid />
+      <DataChart />
     </div>
   );
 }
