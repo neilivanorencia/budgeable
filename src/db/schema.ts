2239de95f054013094aa8dcdd299664054505ee4
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-04-22 09:12:10 +0800

    Create connected banks table schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -61,3 +61,9 @@ export const transactionsRelations = relations(transactions, ({ one }) => ({
 export const transactionsInsertSchema = createInsertSchema(transactions, {
   date: z.coerce.date(),
 });
+
+export const connectedBanks = pgTable("connected_banks", {
+  id: text("id").primaryKey(),
+  userId: text("user_id").notNull(),
+  accessToken: text("access_token").notNull(),
+});
