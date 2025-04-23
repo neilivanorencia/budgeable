dd18c5f4484efbf95d27639ce5cde9ec672ffc61
Neil Ivan Orencia <193382959+neilivanorencia@users.noreply.github.com>
2025-04-23 12:56:40 +0800

    Reset and regenerate initial migration

--- drizzle/0000_budgeable.sql
+++ drizzle/0000_budgeable.sql
@@ -1,3 +1,23 @@
+CREATE TABLE "accounts" (
+	"id" text PRIMARY KEY NOT NULL,
+	"plaid_id" text,
+	"name" text NOT NULL,
+	"user_id" text NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "categories" (
+	"id" text PRIMARY KEY NOT NULL,
+	"plaid_id" text,
+	"name" text NOT NULL,
+	"user_id" text NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "connected_banks" (
+	"id" text PRIMARY KEY NOT NULL,
+	"user_id" text NOT NULL,
+	"access_token" text NOT NULL
+);
+--> statement-breakpoint
 CREATE TABLE "transactions" (
 	"id" text PRIMARY KEY NOT NULL,
 	"amount" integer NOT NULL,
